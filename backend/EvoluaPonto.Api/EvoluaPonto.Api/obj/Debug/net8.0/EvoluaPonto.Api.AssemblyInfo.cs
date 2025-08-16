b939c8ab225075a02445b4415ad2d2f29331cca9
GabrielSLange <gabriellange845@gmail.com>
2025-08-15 21:21:00 -0300

    Alterando o tipo dos arquivos de configuração

--- backend/EvoluaPonto.Api/EvoluaPonto.Api/obj/Debug/net8.0/EvoluaPonto.Api.AssemblyInfo.cs
+++ backend/EvoluaPonto.Api/EvoluaPonto.Api/obj/Debug/net8.0/EvoluaPonto.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EvoluaPonto.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22057c911208216dea958e81bad23271a91abf9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbb544390cdbfeea7321fc8ebbe377f2a2498592")]
 [assembly: System.Reflection.AssemblyProductAttribute("EvoluaPonto.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EvoluaPonto.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
