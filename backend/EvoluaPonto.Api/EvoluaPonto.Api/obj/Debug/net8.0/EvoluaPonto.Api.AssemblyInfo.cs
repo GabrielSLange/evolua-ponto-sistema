ddd629ea70263c2c4108990c9df32e818cab285d
GabrielSLange <gabriellange845@gmail.com>
2025-09-17 22:09:02 -0300

    feat: adicionar configuração do Docker, incluindo Dockerfile e docker-compose para backend e frontend

--- backend/EvoluaPonto.Api/EvoluaPonto.Api/obj/Debug/net8.0/EvoluaPonto.Api.AssemblyInfo.cs
+++ backend/EvoluaPonto.Api/EvoluaPonto.Api/obj/Debug/net8.0/EvoluaPonto.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EvoluaPonto.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcfe0ec3e5064be95b52cedff5f7f9baaa48b1d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e010c968244da1cf0529f2bd5b7f05b415da5db")]
 [assembly: System.Reflection.AssemblyProductAttribute("EvoluaPonto.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EvoluaPonto.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
