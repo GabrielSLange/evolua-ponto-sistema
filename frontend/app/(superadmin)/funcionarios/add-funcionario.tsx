8dcf4fcf5546011879b8de2526359ee2825c9dc1
VitorCMoraes <vitcamo.15@gmail.com>
2025-10-02 00:33:51 -0300

    feat: add AddFuncionarioScreen component for employee management

---
+++ frontend/app/(superadmin)/funcionarios/add-funcionario.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { View } from "react-native";
+import { Appbar } from "react-native-paper";
+import { useRouter, useLocalSearchParams } from "expo-router";
+import FuncionarioForm from "../../../components/forms/FuncionarioForm";
+import { useAddFuncionario } from "@/hooks/superadmin/useFuncionario";
+import ScreenContainer from "@/components/layouts/ScreenContainer";
+
+const AddFuncionarioScreen = () => {
+    const router = useRouter();
+    const { estabelecimentoId, estabelecimentoNome, empresaId } = useLocalSearchParams<{ estabelecimentoId: string; estabelecimentoNome: string; empresaId: string }>(); // Pega o ID do estabelecimento-mãe
+
+    const { loading, addFuncionario } = useAddFuncionario(estabelecimentoId as string, estabelecimentoNome as string);
+
+    return (
+        <ScreenContainer>
+            <View style={{ flex: 1 }}>
+                <Appbar.Header>
+                    <Appbar.BackAction onPress={() => router.push(`/funcionarios?estabelecimentoId=${estabelecimentoId}&estabelecimentoNome=${estabelecimentoNome}`)} />
+                    <Appbar.Content title="Novo Funcionário" />
+                </Appbar.Header>
+                <FuncionarioForm
+                    isLoading={loading}
+                    onSubmit={addFuncionario}
+                    submitButtonLabel="Cadastrar"
+                />
+            </View>
+        </ScreenContainer>
+    );
+}
+
+export default AddFuncionarioScreen;
