9d7e2747a5162a2aec722798c58093a9223d6d19
VitorCMoraes <vitcamo.15@gmail.com>
2025-10-29 23:29:16 -0300

    feat(model): Atualiza ModelRegistroPonto para suportar solicitações de registro manual e acompanhamento de status

---
+++ backend/EvoluaPonto.Api/EvoluaPonto.Api/Models/Enums/StatusSolicitacao.cs
@@ -0,0 +1,10 @@
+namespace EvoluaPonto.Api.Models.Enums
+{
+    // Este enum será armazenado como um 'int' no banco de dados.
+    public enum StatusSolicitacao
+    {
+        Pendente,   // 0
+        Aprovado,   // 1
+        Rejeitado   // 2
+    }
+}
