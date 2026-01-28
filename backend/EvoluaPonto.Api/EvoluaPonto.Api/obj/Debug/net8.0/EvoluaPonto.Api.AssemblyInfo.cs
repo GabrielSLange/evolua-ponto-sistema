5e58cbcb1185a7d519076b29bac83a89f69f7fb6
GabrielSLange <gabriellange845@gmail.com>
2026-01-28 12:31:43 -0300

    feat: adicionar novo domínio à configuração CORS

--- backend/EvoluaPonto.Api/EvoluaPonto.Api/obj/Debug/net8.0/EvoluaPonto.Api.AssemblyInfo.cs
+++ backend/EvoluaPonto.Api/EvoluaPonto.Api/obj/Debug/net8.0/EvoluaPonto.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EvoluaPonto.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0792b26ae02b193ba0dec4339e30331898c6947")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+649d8c0ad0200909abe01adcf25160a7d279ee30")]
 [assembly: System.Reflection.AssemblyProductAttribute("EvoluaPonto.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EvoluaPonto.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
